942485ed2e5d12d18ff0b0932f741f0def6eb285
EtienneLz <louchez.etienne@orange.fr>
2022-01-06 18:49:46 +0100

    testbranch

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: elouchez <elouchez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 12:25:23 by elouchez          #+#    #+#             */
-/*   Updated: 2022/01/05 16:33:14 by elouchez         ###   ########.fr       */
+/*   Updated: 2022/01/06 18:49:18 by elouchez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,11 @@
 static void	reset_var(t_data *data)
 {
 	ft_lstfree(data);
+}
+
+static void	mini_routine(t_data *data)
+{
+	//to be continued...
 }
 
 int	main(int argc, char **argv, char **envp)
