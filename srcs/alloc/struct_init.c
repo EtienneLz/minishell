d35af241f696066cbb3111d2eca4de056dfc1911
EtienneLz <louchez.etienne@orange.fr>
2022-01-24 16:59:25 +0100

    pull

--- srcs/alloc/struct_init.c
+++ srcs/alloc/struct_init.c
@@ -6,7 +6,7 @@
 /*   By: elouchez <elouchez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/28 15:17:12 by elouchez          #+#    #+#             */
-/*   Updated: 2022/01/21 18:02:46 by elouchez         ###   ########.fr       */
+/*   Updated: 2022/01/24 16:50:22 by elouchez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ void	init(t_data *data)
 	data->first = NULL;
 	data->error = 0;
 	data->nb_pipe = 0;
-	data->pipe.tube[0] = 0;
-	data->pipe.tube[1] = 0;
 	data->splitted_args = NULL;
 	data->buffer = "";
 	data->quote_type = '\0';
