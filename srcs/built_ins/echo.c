194db51ab1a7a1696703bf439c108c86e8a68632
pafftek <marie.selignac@gmail.com>
2022-03-08 18:49:24 +0100

    echo flag

--- srcs/built_ins/echo.c
+++ srcs/built_ins/echo.c
@@ -12,6 +12,30 @@
 
 #include "../../includes/minishell.h"
 
+static void	flag_n(char **args, int *i, int *flag)
+{
+	int	j;
+
+	while (args[*i])
+	{
+		if (args[*i][0] == '-')
+		{
+			j = 1;
+			while (args[*i][j] && args[*i][j] == 'n')
+				j++;
+			if (args[*i][j] == '\0')
+			{
+				(*i)++;
+				*flag = 1;
+			}
+			else
+				return ;
+		}
+		else
+			return ;
+	}
+}
+
 int	ft_echo(char **args)
 {
 	int	i;
@@ -19,11 +43,7 @@ int	ft_echo(char **args)
 
 	flag = 0;
 	i = 1;
-	if (!ft_strcmp(args[1], "-n"))
-	{
-		i++;
-		flag = 1;
-	}
+	flag_n(args, &i, &flag);
 	if (args[1] == NULL && flag == 0)
 		ft_putstr_fd("\n", 1);
 	else
